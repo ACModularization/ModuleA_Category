23c89987677ee52813fed27370dc2863b166f77f
张亚浩 <zhangyahao897@pingan.com.cn>
2019-03-26 17:38:26 +0800

    change mediator

--- ModuleACategory/ModuleACategory/CTMediator+ModuleA.h
+++ ModuleACategory/ModuleACategory/CTMediator+ModuleA.h
@@ -1,18 +1,17 @@
 //
-//  ACMediator+ModuleA.h
+//  CTMediator+ModuleA.h
 //  ModuleACategory
 //
 //  Created by 张亚浩 on 2019/3/26.
 //  Copyright © 2019 Ace. All rights reserved.
 //
 
-#import <ACMediator/ACMediator.h>
+#import <CTMediator/CTMediator.h>
 #import <UIKit/UIKit.h>
-
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ACMediator (ModuleA)
+@interface CTMediator (ModuleA)
 
 - (UIViewController *)moduleA_firstViewControllerWithCallback:(void (^)(NSString *))callback;
 
--- ModuleACategory/ModuleACategory/CTMediator+ModuleA.m
+++ ModuleACategory/ModuleACategory/CTMediator+ModuleA.m
@@ -1,16 +1,16 @@
 //
-//  ACMediator+ModuleA.m
+//  CTMediator+ModuleA.m
 //  ModuleACategory
 //
 //  Created by 张亚浩 on 2019/3/26.
 //  Copyright © 2019 Ace. All rights reserved.
 //
 
-#import "ACMediator+ModuleA.h"
+#import "CTMediator+ModuleA.h"
 
 static NSString const *kParamsCallback = @"kParamsCallback";
 
-@implementation ACMediator (ModuleA)
+@implementation CTMediator (ModuleA)
 
 - (UIViewController *)moduleA_firstViewControllerWithCallback:(void (^)(NSString *))callback {
     NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
